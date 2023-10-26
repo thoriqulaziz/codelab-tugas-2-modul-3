5d54cbd271b23946d98fe8d5c4d045077b933d26
thoriqulaziz <127114714+thoriqulaziz@users.noreply.github.com>
2023-10-26 12:29:29 +0700

    Add files via upload

---
+++ src/pages/home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from '../components/navbar'
+import Footer from '../components/footer';
+
+
+ 
+const HomePage = () => {
+    return (
+      <div>
+        <Navbar />
+        <h1>INI HOME</h1>
+       <Footer/>
+      </div>
+    );
+};
+
+export default HomePage;
